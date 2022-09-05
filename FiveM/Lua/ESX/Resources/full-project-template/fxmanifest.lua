3bb0531a874069636b22733927b16c6a6ced79ba
Sören <74495476+1o2r3n@users.noreply.github.com>
2022-09-05 16:44:59 +0200

    Add files via upload

---
+++ FiveM/Lua/ESX/Resources/full-project-template/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author '1o2r3n'
+description 'full-project-template'
+version '0.0.0'
+
+
+ui_page 'interface/ui.html'
+files {
+    'interface/ui.html',
+    'interface/app.js',
+}
+
+client_scripts {
+  '@NativeUI/nativeui.lua',
+  'config/config.lua',
+  'lua/client/main.lua',
+}
+
+server_scripts {
+  '@NativeUI/nativeui.lua',
+  '@mysql-async/lib/MySQL.lua',
+  'config/config.lua',
+  'lua/server/main.lua',
+  'lua/server/checking.lua',
+}
